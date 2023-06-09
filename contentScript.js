9607d1d68ef6387c3a5a6a858c90bcb73fc03ab2
matthewcuan <cuanmatthewl@gmail.com>
2023-06-08 17:25:58 -0700

    removed unnecessary code

--- contentScript.js
+++ contentScript.js
@@ -3,30 +3,9 @@ console.log("scraping")
 // Extract the article title
 const message = document.querySelector("#firstHeading").textContent;
 
-// // Extract the first paragraph
-// // const firstParagraph = document.querySelector("#mw-content-text p").textContent;
-
-// // Create a message object with the extracted content
-// // const message = {
-// //   articleTitle: articleTitle,
-// //   firstParagraph: firstParagraph
-// // };
-
 console.log(message)
 
-// // Send the message to the extension's popup or background script
-
-// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//     if (message.popUpOpened) {
-//         console.log("popup opened");
-//         sendResponse({ received: true });
-//         // chrome.runtime.sendMessage(title);
-//     };
-// });
-
+// send article title to popup
 chrome.runtime.sendMessage(message, (response) => {
     console.log(response)
 });
-
-// console.log("hello world")
-// alert('Hello, world!');
--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // gets current tab
     const currentTab = tabs[0];
 
+    // executes contentScript
     chrome.scripting.executeScript({
       target: {tabId: currentTab.id},
       files: ['contentScript.js']
     });
 
+    // gets popup elements
     console.log(currentTab.id)
     const urlDisplay = document.getElementById("urlDisplay");
     const isWikipediaArticle = isWikipediaURL(currentTab.url);
 
     if (isWikipediaArticle) {
       urlDisplay.textContent = "Summary available: ";
-      // chrome.tabs.sendMessage(currentTab.id, { popupOpened: true });
     } else {
       urlDisplay.textContent = "Not a Wikipedia article";
     }
   });
 
+  // listens for contentScript
   chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     console.log(message);
     sendResponse("Received message in background script:" + message)
