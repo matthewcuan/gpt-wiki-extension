145f6b50632df1563ef3fc8f67b5780b613e0755
matthewcuan <cuanmatthewl@gmail.com>
2023-06-16 11:59:09 -0700

    get all paragraphs in introduction

--- extension/content-script.js
+++ extension/content-script.js
@@ -3,15 +3,22 @@
 try {
     // Extract the article title
     const title = document.querySelector("#firstHeading").textContent;
-    const introHTML = document.querySelector("#mw-content-text .mw-parser-output p:not(.mw-empty-elt)");
-    console.log(introHTML);
-    const intro = introHTML.textContent;
-    console.log(intro);
 
+    // Extract article intro
+    const introHTML = document.querySelector("#mw-content-text .mw-parser-output p:not(.mw-empty-elt)");
+    let intro = introHTML.textContent;
+    let addParagraphsHTML = introHTML.nextElementSibling;
+
+    // Check if there is an additional intro paragraph and add if exists
+    while (addParagraphsHTML.nodeName == "P") {
+        intro += addParagraphsHTML.textContent;
+        addParagraphsHTML = addParagraphsHTML.nextElementSibling;
+    }
+    console.log(intro)
 
     // Send article title to popup
     chrome.runtime.sendMessage({ title : title, intro : intro }, (response) => {
-        console.log(response)
+        console.log(response);
     });
 
     function filterTextFromHTML(html) {
